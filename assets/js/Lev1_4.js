814b9059f4cea2cb923a31efd0620eee22327762
uxvu1196 <uxvu1196@gmail.com>
2021-07-19 18:39:19 +0200

    first commit

---
+++ assets/js/Lev1_4.js
@@ -0,0 +1,43 @@
+// window.alert("Hallo Welt");
+
+// Die Methode alert() zeigt ein Warnfeld mit einer angegebenen Meldung und einer OK-Schaltfläche an.
+
+
+
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+
+// let alter = prompt( "Gib mir bitte dein Alter:" );
+// console.log(alter);
+
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+
+// Cancel = Startwert 
+
+
+// let = Befehl um eine Variable zu definieren
+// prompt = zeigt ein Dialogfenster mit einem Text-Eingabefeld an, mit einer optionalen Nachricht an den Benutzer.
+// console.log = Das erlaubt dir Ausgaben in die Inspektor Konsole zu schreiben - siehe bei Untersuchen
+
+
+
+
+// let b = 5;
+// let a = b * 5 -3;
+// alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// alert(mann + grosse);
+
+
+
+window.confirm("Stimmen Sie meiner Meinung zu?"); 
+
+// Die Methode confirm() zeigt ein Dialogfeld mit einer angegebenen Meldung sowie einer OK- und einer Abbrechen-Schaltfläche an.
+// Ein Bestätigungsfeld wird häufig verwendet, wenn der Benutzer etwas überprüfen oder akzeptieren soll.
+
+
+
+
+
+
